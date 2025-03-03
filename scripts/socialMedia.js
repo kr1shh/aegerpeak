c4c0812961edfa73e7c69de809e9db6779fa7999
Binnukrishna <binnukrishna5233@gmail.com>
2025-03-03 16:07:06 +0530

    udpate : social medias updated

--- scripts/socialMedia.js
+++ scripts/socialMedia.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const fbIcon = document.querySelector(".fb-icon");
     const ytIcon = document.querySelector(".yt-icon");
     const igIcon = document.querySelector(".insta-icon");
+    const liIcon = document.querySelector(".li-icon");
 
     revealBtn.addEventListener("click", ()=>{
         active = !active;
@@ -13,29 +14,35 @@ document.addEventListener("DOMContentLoaded", ()=>{
             fbIcon.style.display = "flex";
             ytIcon.style.display = "flex";
             igIcon.style.display = "flex";
+            liIcon.style.display = "flex";
 
             setTimeout(()=>{
                 fbIcon.style.opacity = "1";
                 ytIcon.style.opacity = "1";
                 igIcon.style.opacity = "1";
+                liIcon.style.opacity = "1";
 
                 fbIcon.style.transform = "translateY(0)";
                 ytIcon.style.transform = "translateY(0)";
                 igIcon.style.transform = "translateY(0)";
+                liIcon.style.transform = "translateY(0)"
                 revealBtn.style.transform = "rotate(180deg)";
             }, 100);
             return;
         }else{
+            liIcon.style.transform = "translateY(210px)"
             fbIcon.style.transform = "translateY(190px)";
             ytIcon.style.transform = "translateY(124px)";
             igIcon.style.transform = "translateY(60px)";
             
             setTimeout(()=>{
+                liIcon.style.opacity = "0";
                 fbIcon.style.opacity = "0";
                 ytIcon.style.opacity = "0";
                 igIcon.style.opacity = "0";
                 
                 setTimeout(()=>{
+                    liIcon.style.display = "none"
                     fbIcon.style.display = "none";
                     ytIcon.style.display = "none";
                     igIcon.style.display = "none";
